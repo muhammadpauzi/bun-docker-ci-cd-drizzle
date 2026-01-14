78052f9faecf00450dd9a082206a05e138905766
Muhammad Pauzi <muhammadpauzi200@gmail.com>
2026-01-14 12:29:03 +0700

    update migrations

--- src/db.ts
+++ src/db.ts
@@ -7,8 +7,8 @@ export const client = new SQL({
   url: Bun.env.DATABASE_URL!,
   // Pengaturan Pool untuk VPS 2GB
   max: 10, // Batas maksimal koneksi simultan (sesuai max_connections di Docker)
-  idleTimeout: 30, // Tutup koneksi nganggur setelah 30 detik untuk hemat RAM
-  connectTimeout: 5, // Gagal jika tidak konek dalam 5 detik
+  idleTimeout: 3000, // Tutup koneksi nganggur setelah 30 detik untuk hemat RAM
+  connectTimeout: 5000, // Gagal jika tidak konek dalam 5 detik
 });
 
 const relations = defineRelations(
